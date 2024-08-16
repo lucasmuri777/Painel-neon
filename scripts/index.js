e1094e36897eda94a148b404e9a4c39ede10c945
lucasmuri777 <lucasmuri777@gmail.com>
2024-08-16 12:10:03 -0300

    2.0

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,36 @@ let colorInput = document.querySelector('#color');
 let sizeInput = document.querySelector('#size');
 let generateButton = document.querySelector('#generate');
 let tamanhoText = document.querySelector('#actual');
+let btnOnOffEffect = document.querySelector('#on-off-effect');
+let alignInputs = document.querySelectorAll('.align-forms input');
+let onOffEffect = true;
 
 let textoTela = document.querySelector('#tela h2');
 
-
+/* Função que muda o alinhamento do texto */
+alignInputs.forEach((input)=>{
+    input.addEventListener('change',()=>{
+        if(input.value == 'left'){
+            textoTela.style.textAlign = 'left';
+        }else if(input.value == 'center'){
+            textoTela.style.textAlign = 'center';
+        }else if(input.value == 'right'){
+            textoTela.style.textAlign = 'right';
+        }
+    })
+})
+/* Função que ativa e desativa o efeito neon */
+btnOnOffEffect.addEventListener('click',()=>{
+    if(onOffEffect){
+        btnOnOffEffect.classList.remove('active');
+        textoTela.classList.add('off');
+        onOffEffect = false;
+    }else{
+        btnOnOffEffect.classList.add('active');
+        textoTela.classList.remove('off');
+        onOffEffect = true;
+    }
+})
 /* Função que muda o conteudo do texto */
 textoInput.addEventListener('keyup',(e)=>{
     if(e.target.value.trim() == ""){
@@ -33,5 +59,5 @@ colorInput.addEventListener('change',(e)=>{
 /*Função que muda o tamanho*/
 sizeInput.addEventListener('change',(e)=>{
     document.documentElement.style.setProperty('--neon-size', e.target.value + 'px');
-    tamanhoText.innerHTML = e.target.value + 'M';
+    tamanhoText.innerHTML = e.target.value + 'CM';
 })
